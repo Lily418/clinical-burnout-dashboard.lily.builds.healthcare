f1e25b200ee64c975908826d360f62b9e6781b73
Lily Hoskin <lily@helixcentre.com>
2020-02-18 19:56:00 +0000

    Add target lines

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,75 @@
 <script>
   import { onMount } from "svelte";
   import { DateTime } from "luxon";
-  import "chartist-plugin-targetline";
 
   let chart;
 
   onMount(() => {
+    Chartist.plugins = {
+      ctTargetLine: function(options) {
+        const defaultOptions = {
+          class: "ct-target-line",
+          color: "blue",
+          label: "",
+          value: null
+        };
+
+        options = Chartist.extend({}, defaultOptions, options);
+
+        return function ctTargetLine(chart) {
+          function projectY(chartRect, bounds, value) {
+            return chartRect.y1 - (chartRect.height() / bounds.max) * value;
+          }
+
+          chart.on("created", function(context) {
+            var targetLineY = projectY(
+              context.chartRect,
+              context.bounds,
+              options.value
+            );
+
+            if (options.showOverlay) {
+              context.svg.elem(
+                "rect",
+                {
+                  width: context.chartRect.x2 - context.chartRect.x1 + 5,
+                  height: targetLineY - context.chartRect.y2,
+                  x: context.chartRect.x1,
+                  y: context.chartRect.y2,
+                  style: `fill: ${options.color}; opacity: 0.075; z-index: -5;`
+                },
+                options.class
+              );
+            }
+
+            context.svg.elem(
+              "line",
+              {
+                x1: context.chartRect.x1,
+                x2: context.chartRect.x2,
+                y1: targetLineY,
+                y2: targetLineY,
+                style: `stroke: ${options.color}`
+              },
+              options.class
+            );
+
+            context.svg
+              .elem(
+                "text",
+                {
+                  x: context.chartRect.x2,
+                  y: targetLineY + 20,
+                  style: `text-anchor: end; fill: ${options.color};`
+                },
+                ""
+              )
+              .text(options.label);
+          });
+        };
+      }
+    };
+
     console.log(Chartist.plugins);
 
     const days = [...Array(14).keys()]
@@ -23,12 +87,6 @@
       series: [
         {
           value: [16, 11, 17, 15, 16, 14, 12, 12, 15, 12, 12, 15, 15, 17]
-        },
-        {
-          value: [7, 8, 7, 10, 11, 11, 6, 11, 6, 9, 7, 6, 6, 11]
-        },
-        {
-          value: [7, 8, 7, 10, 11, 11, 6, 11, 6, 9, 7, 6, 6, 11]
         }
       ]
     };
@@ -36,27 +94,30 @@
     // Create a new line chart object where as first parameter we pass in a selector
     // that is resolving to our chart container element. The Second parameter
     // is the actual data object.
-    new Chartist.Line(
-      chart,
-      data,
-      {
-        high: 20,
-        low: 0,
-        axisX: {
-          showGrid: true
-        },
-        axisY: {
-          onlyInteger: true
-        }
+    new Chartist.Line(chart, data, {
+      high: 20,
+      low: 0,
+      axisX: {
+        showGrid: true
       },
-      {
-        plugins: [
-          Chartist.plugins.ctTargetLine({
-            value: 4
-          })
-        ]
-      }
-    );
+      axisY: {
+        onlyInteger: true
+      },
+      plugins: [
+        Chartist.plugins.ctTargetLine({
+          color: "#e84747",
+          label: "Standardised Definition of Burnout",
+          value: 13,
+          showOverlay: true
+        }),
+        Chartist.plugins.ctTargetLine({
+          color: "#E847E8",
+          label: "National Average",
+          value: 15,
+          showOverlay: false
+        })
+      ]
+    });
   });
 </script>
 
@@ -67,10 +128,19 @@
   }
 </style>
 
-<h1>Results Dashboard</h1>
+<h1>Burnout Scale Results (Weekly)</h1>
 <h2>Imperial College NHS</h2>
-<h3>Burnout Scale Results</h3>
 <div bind:this={chart} class="ct-chart ct-perfect-fourth" />
 <p>
   <em>Data for illustrative purposes only</em>
+</p>
+<h3>About this data</h3>
+<p>
+  Etiam aliquam cubilia fringilla lacus turpis nisi rhoncus tempus, senectus
+  ridiculus accumsan molestie malesuada magna potenti justo tincidunt, facilisi
+  sed consequat sem taciti ante leo. Tempor ultricies placerat dui neque
+  volutpat vehicula morbi augue porta ut lectus, a sagittis eros mus consequat
+  ac nulla aenean leo vivamus. Augue quam lacinia pharetra enim donec dictum in,
+  habitant molestie tempus dignissim urna fusce imperdiet, tellus aliquam ac
+  bibendum vehicula blandit.
 </p>
